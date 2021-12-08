0cd6ec9d2697ffa8bcb88e1537e36fb6ba1a749d
dougdougdougdougdougdoug <93460252+dougdougdougdougdougdoug@users.noreply.github.com>
2021-12-08 12:44:49 -0500

    Add files
    Adding additional files for weather patterns and fitting CSS issue detected in W3C

--- script.js
+++ script.js
@@ -16,9 +16,26 @@ button.addEventListener('click', function(){
         temperature.innerHTML = temperatureValue + " C";
         condition.innerHTML = conditionValue;
         zipCode.value = "";
-        if(conditionID >= 701){
+        if(conditionID >= 300 && conditionID <= 531){
             document.body.style.backgroundImage = "url('rain.jpeg')";
         }
+        else if(conditionID >= 200 && conditionID <= 232){
+            document.body.style.backgroundImage = "url('thunder.jpeg')";
+        }
+        else if(conditionID >= 600 && conditionID <= 622){
+            document.body.style.backgroundImage = "url('snow.jpeg')";
+        }
+        else if(conditionID >= 700 && conditionID <= 781){
+            document.body.style.backgroundImage = "url('smokemist.jpeg')";
+        }
+        else if(conditionID == 800){
+            document.body.style.backgroundImage = "url('clearsky.jpeg')";
+        }
+        else if(conditionID >= 801 && conditionID <= 804){
+            document.body.style.backgroundImage = "url('cloudy.jpeg')";
+        }
+        
+
     })
 
 
